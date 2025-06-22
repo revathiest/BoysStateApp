235992606c3132acd913c1f5ae1e43b3e7743765
revathiest <revathiest@gmail.com>
2025-06-21 21:21:37 -0500

    Switch tests to jest

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,9 +1,7 @@
-import test from 'node:test';
-import assert from 'node:assert';
 import { createElement } from 'react';
 import App from '../App.js';
 
 test('App renders root view', () => {
   const element = createElement(App);
-  assert.strictEqual(element.type, App);
+  expect(element.type).toBe(App);
 });
