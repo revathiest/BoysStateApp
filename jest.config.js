39dc3ec91311393fd4148244a7aed3a71da88dea
revathiest <revathiest@gmail.com>
2025-06-21 21:44:29 -0500

    Updated jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,30 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   setupFiles: ['<rootDir>/jest.setup.js'],
+
+  // === COVERAGE SETTINGS === //
+  collectCoverage: true,                        // always gather coverage
+  coverageDirectory: 'coverage',               // output folder
+  coverageReporters: ['text', 'lcov'],         // text = console table, lcov for HTML
+  collectCoverageFrom: [
+    '*.{js,ts}',
+    'assets/*.{js,ts}',
+    'components/*.{js,ts}',
+    'screens/*.{js,ts}',
+    '!*.config.js',
+    '!**/node_modules/**',
+    '!**/__mocks__/**'
+  ],
+    "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+  // === SILENT MODE ===
+  verbose: false,
+  silent: true,
+  reporters: ['default']
 };
